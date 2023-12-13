8501de7af682bb29012e742a6f998420866018a8
enoyat <50427196+enoyat@users.noreply.github.com>
2023-12-13 09:12:31 +0700

    1

---
+++ servis.sql
@@ -0,0 +1,211 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 13, 2023 at 03:12 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `servis`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `merk`
+--
+
+DROP TABLE IF EXISTS `merk`;
+CREATE TABLE IF NOT EXISTS `merk` (
+  `idmerk` int(4) NOT NULL AUTO_INCREMENT,
+  `jenismerk` text NOT NULL,
+  PRIMARY KEY (`idmerk`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+DROP TABLE IF EXISTS `migrations`;
+CREATE TABLE IF NOT EXISTS `migrations` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `motor`
+--
+
+DROP TABLE IF EXISTS `motor`;
+CREATE TABLE IF NOT EXISTS `motor` (
+  `idmotor` int(10) NOT NULL AUTO_INCREMENT,
+  `iduser` int(11) NOT NULL,
+  `nopolisi` varchar(10) NOT NULL,
+  `idmerk` int(4) NOT NULL,
+  `jenismerk` text DEFAULT NULL,
+  PRIMARY KEY (`idmotor`),
+  KEY `iduser` (`iduser`),
+  KEY `idmerk` (`idmerk`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+DROP TABLE IF EXISTS `password_resets`;
+CREATE TABLE IF NOT EXISTS `password_resets` (
+  `email` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  KEY `password_resets_email_index` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `password_resets`
+--
+
+INSERT INTO `password_resets` (`email`, `token`, `created_at`) VALUES
+('enoyat@gmail.com', '$2y$10$GwgTxsD7YCWGKf11JW0KKeOmadlNSHzF8UrSNoZ93ZSvikRAeRumu', '2023-07-02 12:04:59');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pelayanan`
+--
+
+DROP TABLE IF EXISTS `pelayanan`;
+CREATE TABLE IF NOT EXISTS `pelayanan` (
+  `idpelayanan` int(11) NOT NULL AUTO_INCREMENT,
+  `iduser` int(11) NOT NULL,
+  `idmotor` int(11) NOT NULL,
+  `pelayanan1` tinyint(1) NOT NULL,
+  `pelayanan2` tinyint(1) NOT NULL,
+  `pelayanan3` tinyint(1) NOT NULL,
+  `pelayanan4` tinyint(1) NOT NULL,
+  `pelayanan5` tinyint(1) NOT NULL,
+  `pelayanan6` tinyint(1) NOT NULL,
+  `pelayanan7` tinyint(1) NOT NULL,
+  `pelayananlain` text NOT NULL,
+  PRIMARY KEY (`idpelayanan`),
+  KEY `iduser` (`iduser`),
+  KEY `idmotor` (`idmotor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reservasi`
+--
+
+DROP TABLE IF EXISTS `reservasi`;
+CREATE TABLE IF NOT EXISTS `reservasi` (
+  `idreservasi` int(11) NOT NULL AUTO_INCREMENT,
+  `idpelayanan` int(11) NOT NULL,
+  `idmekanik` int(11) NOT NULL,
+  PRIMARY KEY (`idreservasi`),
+  KEY `idpelayanan` (`idpelayanan`),
+  KEY `idmekanik` (`idmekanik`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+CREATE TABLE IF NOT EXISTS `roles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_name` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`id`, `role_name`, `created_at`, `updated_at`) VALUES
+(1, 'administrator', '2020-12-22 02:12:09', '2020-12-22 02:12:09'),
+(2, 'mekanik', '2020-12-22 02:12:09', '2020-12-22 02:12:09'),
+(3, 'customer', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `alamat` text DEFAULT NULL,
+  `nohp` text DEFAULT NULL,
+  `roles_id` int(2) NOT NULL,
+  `access_token` text DEFAULT NULL,
+  `remember_token` text DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  KEY `roles_id` (`roles_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=130 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `alamat`, `nohp`, `roles_id`, `access_token`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'admin', 'admin@gmail.com', '$2y$10$BDC0RyJgj7izd3Fs4.8b5.f1No7tnLVRzEwtWVzb9Y2H9ckKYLFka', NULL, NULL, 1, NULL, NULL, '2023-07-18 08:21:03', '2023-07-18 08:21:03');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `motor`
+--
+ALTER TABLE `motor`
+  ADD CONSTRAINT `motor_ibfk_1` FOREIGN KEY (`idmerk`) REFERENCES `merk` (`idmerk`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `pelayanan`
+--
+ALTER TABLE `pelayanan`
+  ADD CONSTRAINT `pelayanan_ibfk_1` FOREIGN KEY (`idpelayanan`) REFERENCES `reservasi` (`idpelayanan`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `pelayanan_ibfk_2` FOREIGN KEY (`idmotor`) REFERENCES `motor` (`idmotor`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `pelayanan_ibfk_3` FOREIGN KEY (`iduser`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`roles_id`) REFERENCES `roles` (`id`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
